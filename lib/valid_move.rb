94502b8e659606a0dbea6b8257d13b0eb6b61515
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-02 12:19:34 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    if position_taken?
+    if position_taken?(board, index)
       return "index is not in board"
     else
       return "index is not in board"
