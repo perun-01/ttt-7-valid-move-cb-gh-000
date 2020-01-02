d8b19a230725eea9bb28adcfd6ea654bfb61db88
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-02 12:50:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,11 @@
 def valid_move?(board, index)
     if index<=8
       if board[index]== " " || board[index]== ""
-        return false
-      elsif board[index]== nil
-        return false
-      elsif board[index]== "X" || board[index]== "O"
         return true
+      elsif board[index]== nil
+        return true
+      elsif board[index]== "X" || board[index]== "O"
+        return false
       end
     elsif index<=8
       return false
