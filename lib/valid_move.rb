ebb5c060f1afbd0750f4b58901d1a65aee0df2cf
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-02 12:08:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?
-  
+
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
@@ -8,7 +8,6 @@ def position_taken?(board, index)
   if board[index] == " " || ""
     return false
   elsif board[index] == "X" || "O"
-    
-    
+
+
   end
-  
